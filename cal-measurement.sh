d043f269f0fc7aa504755f1e8178cd9b38b172d0
Jingyao <cloudyao95@gmail.com>
2023-09-04 07:08:57 +0000

    update cal-measurement.sh

--- cal-measurement.sh
+++ cal-measurement.sh
@@ -1,1 +1,1 @@
-sev-snp-measure --mode snp --vcpus=$QEMU_CPU_NUM --vcpu-type=$QEMU_CPU_MODEL --ovmf=OVMF.fd --kernel=vmlinuz --initrd=initrd.img > output.txt
+sev-snp-measure --mode snp --vcpus=$QEMU_CPU_NUM --vcpu-type=$QEMU_CPU_MODEL --ovmf=$PWD/snp/setup/non-upm/AMDSEV/ovmf/Build/AmdSev/DEBUG_GCC5/FV/OVMF.fd --kernel=$PWD/snp/setup/non-upm/AMDSEV/linux/guest/debian/linux-image/boot/vmlinuz-5.19.0-rc6-snp-guest-c4daeffce --initrd=$PWD/snp/launch/snp-guest.img > output.txt
